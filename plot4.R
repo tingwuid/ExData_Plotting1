b3886e8fa95dc1d5946fb898cef2b001e7839cc3
tingwuid <tingwuid@gmail.com>
2016-05-15 19:11:36 -0700

    updated plot 4 code

--- plot4.R
+++ plot4.R
@@ -3,7 +3,7 @@ fileName <- "household_power_consumption.txt"
 data <- read.table(text=grep("^[1,2]/2/2007",readLines(fileName),value = TRUE),sep = ";",header = TRUE, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),na.strings="?")
 time <- strptime(paste(data$Date,data$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
 data <- cbind(time, data)
-par(mfrow = c(2,2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+par(mfrow = c(2,2), mar = c(4, 4, 2, 1), oma = c(0, 0, 1, 0))
 
 
 with(data, {
